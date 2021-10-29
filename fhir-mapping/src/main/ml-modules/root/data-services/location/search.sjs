f79cfd515923d0cfcaf48f01050f6f0d600c8376
Brian Griffin <brian.griffin@marklogic.com>
2021-10-29 10:48:15 -0600

    Consolidate location search into a single data service function

--- fhir-mapping/src/main/ml-modules/root/data-services/location/search.sjs
+++ fhir-mapping/src/main/ml-modules/root/data-services/location/search.sjs
@@ -1,73 +1,171 @@
 'use strict';
 const egress = require("/fhir-accelerator/egress-mapping.sjs")
+const op = require('/MarkLogic/optic');
 
-// search egress practitioner
+const beginningOfDay = xs.time('00:00:00')
+
+function applyStringParamModifier(modifier, value) {
+  if("exact" === criteria.modifier) {
+    //do nothing to the value
+  } else if("contains" === criteria.modifier) {
+    value = '%' + value + '%'
+  } else if("" === criteria.modifier) {
+    value = value + '%'
+  }
+  
+  return value;
+}
+
+function convertDateParameterToDateTimeQuery(columns, criteria, localConditionList) {
+  var valueCriteria = []
+  for(var value of criteria.values) {
+    let columnCriteria = []
+    let valueDateTime = fn.dateTime(xs.date(value), beginningOfDay)
+    let valueNextDayDateTime = fn.dateTime(xs.date(value).add(xs.dayTimeDuration('P1D')), beginningOfDay)
+    
+    for (var col of columns) {
+      
+      switch (criteria.modifier) {
+        case "=":
+          columnCriteria.push(op.and(op.ge(col, valueDateTime), op.lt(col, valueNextDayDateTime)))
+          break;
+        case ">":
+          columnCriteria.push(op.ge(col, valueNextDayDateTime))
+          break;
+        case ">=":
+          columnCriteria.push(op.ge(col, valueDateTime))
+          break;
+        case "<":
+          columnCriteria.push(op.lt(col, valueDateTime))
+          break;
+        case "<=":
+          columnCriteria.push(op.lt(col, valueNextDayDateTime))
+          break;
+      }
+    }
+    addFilterToConditionList(valueCriteria, columnCriteria)
+  }
+  addFilterToConditionList(localConditionList, valueCriteria)
+}
+
+function convertStringParameterToQuery(columns, criteria, localConditionList) {
+  var valueCriteria = []
+  for(var value of criteria.values) {
+    var columnCriteria = []
+    //adjust the value based on the modifier
+    var value = applyStringParamModifier(criteria.modifier, value)
+    for (var col of columns) {
+      columnCriteria.push(op.sql.like(col, value))
+    }
+    addFilterToConditionList(valueCriteria, columnCriteria)
+  }
+  addFilterToConditionList(localConditionList, valueCriteria)
+}
+
+function addFilterToConditionList(conditions, newFilter) {
+  if(newFilter.length > 1) {
+    conditions.push(op.or(...newFilter))
+  } else {
+    conditions.push(newFilter[0])
+  }
+}
+
+// search for locations. null or empty means no filters
 var search;
 
-// starting position for paginated search results.
-// currently not implemented.
+// starting position for paginated search results. null means start at 0
 var start;
 
-// number of results per paginated search results.
-// currently not implemented.
+// number of results per paginated search results. null means return all results
 var limit;
 
-const fieldMap = new Map([
-  ['address-city', 'city'],
-  ['address-state', 'state'],
-  ['address-postalcode', 'zip'],
-  ['identifier', 'id'], // TODO: This isn't really supposed to be ID
-  ['id', 'id'],
-  ['_id', 'id'],
-  ['name', 'name'],
-  ['type', 'addresstype'],
-  ['address', ["name", "line1", "line2", "line3", "city", "county", "countyCode", "state","zip","addresstype"]]
-]);
-
 const searchList = search ? JSON.parse(search) : [];
-const options = ["case-insensitive", "wildcarded", "whitespace-insensitive", "punctuation-insensitive"];
 
-// search for and filter your documents if needed
-const query = cts.andQuery([
-  cts.collectionQuery('provider-dhhs-canonical'),
-  cts.jsonPropertyValueQuery("providerType", "PERSON"),
-  ...searchList.map(({ field, modifier, values }) => {
-    const searchValues = egress.searchValuesWithModifier(values, modifier)
-    return cts.jsonPropertyValueQuery(fieldMap.get(field), searchValues, options)
-  })
-]);
+const locationCol = op.col('location');
+const idCol = op.col('id');
 
-// do the search
-const searchResults = cts.search(query);
-// Apply paging logic
-const rawDocs = fn.subsequence(searchResults, start, limit)
-// Extract matching locations
-var locations = [];
-var loopCount = 1
-for (var rawDoc of rawDocs) {
-  for (const loc of rawDoc.xpath("//providerLocations")) {
-    if (cts.contains(loc, cts.andQuery([
-            ...searchList.map(({ field, modifier, values }) => {
-              const searchValues = egress.searchValuesWithModifier(values, modifier)
-              return cts.jsonPropertyValueQuery(fieldMap.get(field), searchValues, options)
-            })])
-      )) {
-        if (locations.length <= limit && loopCount <= limit) {
-          if (loopCount >= start) {
-            locations.push(loc);            
-          }
-          loopCount++;
-        } else {
-          break;
-        }
-    }
-  };
-}// standard transform on searchResults variable
-const result = egress.transformMultiple(locations, "ProviderToFHIRLocation");
+const cityCol = op.col('city');
+const stateCol = op.col('state');
+const zipCol = op.col('postalcode');
+const line1Col = op.col('line1');
+const line2Col = op.col('line2');
+const line3Col = op.col('line3');
 
-const results = {
-  "results": result
-};
+const lastUpdatedCol = op.col('lastupdated');
 
-// return the result
-results;
+var conditionList = []
+
+// cycle through each search parameter
+for(var criteria of searchList) {
+  switch(criteria.field) {
+    case "id":
+    case "_id":
+      var valueCriteria = []
+      
+      for(var value of criteria.values) {
+        valueCriteria.push(op.sql.like(idCol, value))
+      }
+      
+      addFilterToConditionList(conditionList, valueCriteria, conditionList)
+      
+      break;
+    case "address-city":
+      convertStringParameterToQuery([cityCol], criteria, conditionList)
+      break;
+    case "address-state":
+      convertStringParameterToQuery([stateCol], criteria, conditionList)
+      break;
+    case "address-postalcode":
+      convertStringParameterToQuery([zipCol], criteria, conditionList)
+      break;
+    case "address":
+    case "name":
+      convertStringParameterToQuery([cityCol, stateCol, zipCol, line1Col, line2Col, line3Col], criteria, conditionList)
+      break;
+    case "_lastUpdated":
+      convertDateParameterToDateTimeQuery([lastUpdatedCol], criteria, conditionList)
+  }
+}
+
+const locationdocid = op.fragmentIdCol('locationdocid');
+
+var queryPlan = op.fromView('FHIR', 'Location', null, locationdocid)
+
+xdmp.log("Search" + search, 'notice')
+xdmp.log("Conditions" + conditionList.toString(), 'notice')
+
+if(conditionList.length > 1) {
+  queryPlan = queryPlan.where(op.and(...conditionList))
+} else if (conditionList.length === 1) {
+  queryPlan = queryPlan.where(conditionList[0])
+}
+
+
+
+if(start > 0) {
+  queryPlan = queryPlan.offset(start-1)
+}
+
+if(limit > 0) {
+  queryPlan = queryPlan.limit(limit)
+}
+
+queryPlan = queryPlan.joinDoc(op.col('doc'), locationdocid)
+
+queryPlan = queryPlan.select(['id', 'index', 'doc'], "")
+
+xdmp.log("QueryPlan: " + queryPlan.toString(), 'notice')
+
+let queryResults = queryPlan.result()
+
+xdmp.log("QueryResult: " + queryResults.toString(), 'notice')
+
+let rawResults = []
+
+for(var current of queryResults) {
+   rawResults.push(current.doc.xpath('/envelope/instance/provider/providerLocations').toArray()[current.index-1])
+}
+
+let result = egress.transformMultiple(rawResults, "ProviderToFHIRLocation");
+
+result
--- hapi-server/src/main/java/com/example/fhirexample/ResourceProvider/LocationResourceProvider.java
+++ hapi-server/src/main/java/com/example/fhirexample/ResourceProvider/LocationResourceProvider.java
@@ -3,13 +3,14 @@ package com.example.fhirexample.ResourceProvider;
 import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.annotation.*;
 import ca.uhn.fhir.rest.api.Constants;
-import ca.uhn.fhir.rest.param.DateRangeParam;
+import ca.uhn.fhir.rest.param.DateParam;
 import ca.uhn.fhir.rest.param.StringAndListParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hl7.fhir.dstu2.model.IdType;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Location;
@@ -70,28 +71,31 @@ public class LocationResourceProvider implements IResourceProvider {
 
     @Search
     public List<Location> search(
-            @OptionalParam(name = Location.SP_IDENTIFIER) StringAndListParam identifier,
+            @OptionalParam(name = Location.SP_RES_ID) StringAndListParam id,
+            @OptionalParam(name = Constants.PARAM_LASTUPDATED) DateParam lastUpdated,
             @OptionalParam(name = Location.SP_NAME) StringAndListParam name,
-            @OptionalParam(name = Location.SP_TYPE) StringAndListParam type,
             @OptionalParam(name = Location.SP_ADDRESS_CITY) StringAndListParam city,
             @OptionalParam(name = Location.SP_ADDRESS_STATE) StringAndListParam state,
             @OptionalParam(name = Location.SP_ADDRESS_POSTALCODE) StringAndListParam zip,
+            @OptionalParam(name = Location.SP_ADDRESS) StringAndListParam address,
             @Offset Integer offset,
             @Count Integer count) {
         List<Location> locations = new ArrayList<Location>();
         Pagination page = new Pagination(offset, count);
 
         List<SearchCriteria> searchTerms = new ArrayList<SearchCriteria>();
-        searchTerms.addAll(searchCriteria(Location.SP_IDENTIFIER, identifier));
+        searchTerms.addAll(searchCriteria(Location.SP_RES_ID, id));
+        if(lastUpdated != null)
+            searchTerms.add(searchCriteria(Constants.PARAM_LASTUPDATED, lastUpdated));
         searchTerms.addAll(searchCriteria(Location.SP_NAME, name));
-        searchTerms.addAll(searchCriteria(Location.SP_TYPE, type));
         searchTerms.addAll(searchCriteria(Location.SP_ADDRESS_CITY, city));
         searchTerms.addAll(searchCriteria(Location.SP_ADDRESS_STATE, state));
         searchTerms.addAll(searchCriteria(Location.SP_ADDRESS_POSTALCODE, zip));
+        searchTerms.addAll(searchCriteria(Location.SP_ADDRESS, address));
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode params = objectMapper.valueToTree(searchTerms);
-            JsonNode rootNode = MLSearch.on(thisClient).search(params, page.getOffset(), page.getCount());
+            System.out.println(params.toString());
             ArrayNode rootNode = LocationSearch.on(thisClient).search(params, page.getOffset(), page.getCount());
             locations = getMLLocations(rootNode);
         } catch (Exception ex) {
@@ -100,116 +104,23 @@ public class LocationResourceProvider implements IResourceProvider {
         return locations;
     }
 
-    @Search
-    public List<Location> findLocationsById(
-            @RequiredParam(name = Location.SP_RES_ID) StringAndListParam id,
-            @Offset Integer theOffset,
-            @Count Integer theCount) {
-        List<Location> locations = new ArrayList<Location>();
-        Pagination page = new Pagination(theOffset, theCount);
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<SearchCriteria> searchTerms = new ArrayList<SearchCriteria>();
-            searchTerms.addAll(searchCriteria(Location.SP_RES_ID, id));
-            JsonNode params = objectMapper.valueToTree(searchTerms);
-            System.out.println("PARAMS:" + params.toString());
-            JsonNode rootNode = MLSearch.on(thisClient).searchById(params, page.getOffset(), page.getCount());
-            locations = getMLLocations(rootNode);
-        } catch (Exception ex) {
-            throw new ResourceNotFoundException(ex.getMessage());
+    private Location getMLLocation(ArrayNode rootNode) {
+        if(rootNode.size() > 1) {
+            throw new RuntimeException("Too many documents returned for a single read");
         }
-        return locations;
-    }
 
-    @Search
-    public List<Location> findLocationsByLastUpdated(
-            @RequiredParam(name=Constants.PARAM_LASTUPDATED) DateRangeParam lastUpdated,
-            @Offset Integer theOffset,
-            @Count Integer theCount) {
-        List<Location> locations = new ArrayList<Location>();
-        Pagination page = new Pagination(theOffset, theCount);
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<SearchCriteria> searchTerms = new ArrayList<SearchCriteria>();
-            searchTerms.addAll(searchCriteria(Constants.PARAM_LASTUPDATED, lastUpdated));
-            JsonNode params = objectMapper.valueToTree(searchTerms);
-            System.out.println("PARAMS:" + params.toString());
-            JsonNode rootNode = MLSearch.on(thisClient).searchByDate(params, page.getOffset(), page.getCount());
-            locations = getMLLocations(rootNode);
-        } catch (Exception ex) {
-            throw new ResourceNotFoundException(ex.getMessage());
-        }
-        return locations;
-    }
-
-    @Search()
-    public List<Location> searchByLocationAddress(
-            @RequiredParam(name=Location.SP_ADDRESS) StringAndListParam theAddressParts,
-            @Offset Integer theOffset,
-            @Count Integer theCount) {
-
-        // StringAndListParam is a container for 0..* StringOrListParam, which is in turn a
-        // container for 0..* strings. It is a little bit weird to understand at first, but think of the
-        // StringAndListParam to be an AND list with multiple OR lists inside it. So you will need
-        //
-        System.out.println("ADDRESS PARTS:" + theAddressParts.toString());
-        List<Location> locations = new ArrayList<Location>();
-        Pagination page = new Pagination(theOffset, theCount);
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            List<SearchCriteria> searchTerms = new ArrayList<SearchCriteria>();
-            searchTerms.addAll(searchCriteria(Location.SP_ADDRESS, theAddressParts));
-            JsonNode params = objectMapper.valueToTree(searchTerms);
-            System.out.println("PARAMS:" + params.toString());
-            JsonNode rootNode = MLSearch.on(thisClient).search(params, page.getOffset(), page.getCount());
-            locations = getMLLocations(rootNode);
-        } catch (Exception ex) {
-            throw new ResourceNotFoundException(ex.getMessage());
-        }
-        return locations;
-    }
-
-    private Location getMLLocation(JsonNode rootNode) {
-        Location thisLocation = null;
-        if (rootNode != null) {
-            Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
-            JsonNode docNode = null;
-            while (fieldsIterator.hasNext()) {
-                Map.Entry<String,JsonNode> field = fieldsIterator.next();
-                //docNode = field.getValue().get(0);
-                System.out.println("docSize:" + field.getValue().size());
-                for (int i=0; i < field.getValue().size(); i++) {
-                    docNode = field.getValue().get(i);
-                    if (docNode != null && docNode.isContainerNode()) {
-                        // Parse it
-                        thisLocation = thisParser.parseResource(Location.class, docNode.toString());
-                    }
-                }
-            }
-        }
+        Location thisLocation = thisParser.parseResource(Location.class, rootNode.get(0).toString());
         return thisLocation;
     }
 
-    private List<Location> getMLLocations(JsonNode rootNode) {
+    private List<Location> getMLLocations(ArrayNode rootNode) {
         List<Location> locations = new ArrayList<Location>();
-        Location thisLocation = null;
-        if (rootNode != null) {
-            Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
-            JsonNode docNode = null;
-            while (fieldsIterator.hasNext()) {
-                Map.Entry<String, JsonNode> field = fieldsIterator.next();
-                //docNode = field.getValue().get(0);
-                System.out.println("docSize:" + field.getValue().size());
-                for (int i = 0; i < field.getValue().size(); i++) {
-                    docNode = field.getValue().get(i);
-                    if (docNode != null && docNode.isContainerNode()) {
-                        // Parse it
-                        thisLocation = thisParser.parseResource(Location.class, docNode.toString());
-                        locations.add(thisLocation);
-                    }
-                }
-            }
+
+        for(JsonNode docNode : rootNode) {
+            Location thisLocation = thisParser.parseResource(Location.class, docNode.toString());
+            locations.add(thisLocation);
         }
+
         return locations;
     }
 }
